7bde4bb29176be2c89dcf696f8ffae2588f6b941
Daniil Yarockii <yaroc.d@gmail.com>
2021-06-05 21:13:21 +0300

    coments are added

--- movement_laws.py
+++ movement_laws.py
@@ -2,6 +2,12 @@ import numpy
 
 
 class LinearLaw:
+    """
+    Закон линейного перемещения объекта.
+    В качестве начальных параметров инициализируются:
+    1. коэффициент угла наклона k
+    2. смещение b
+    """
     def __init__(self):
         self.k = numpy.random.uniform(0, 5)
         self.b = numpy.random.uniform(0, 10)
