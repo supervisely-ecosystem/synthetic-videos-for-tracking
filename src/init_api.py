78311569c432f8ef22714600c071d999d5b2fb60
Daniil Yarockii <yaroc.d@gmail.com>
2021-06-10 17:55:40 +0300

    upload function added
    Former-commit-id: 0b0c823b93489b68cb328e5d32f9135eb722dafe
    Former-commit-id: ab2be0834d2d71edf981cfece3003c53e3b56515
    Former-commit-id: 800ea71740cf0efb04260f41f9fe0d6d50ee0b15
    Former-commit-id: fcfa4374bcfaa36756b30318930c7db9e95bb44d

---
+++ src/init_api.py
@@ -0,0 +1,9 @@
+import os
+import supervisely_lib as sly
+
+my_app = sly.AppService()
+
+TEAM_ID = int(os.environ['context.teamId'])
+WORKSPACE_ID = int(os.environ['context.workspaceId'])
+
+api: sly.Api = my_app.public_api
