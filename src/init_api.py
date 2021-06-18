37e5a9ea05a86cdb9ca9df02283161525cc24826
cxnt <pavelbartsits@gmail.com>
2021-06-18 15:39:50 +0300

    ui wip

--- src/init_api.py
+++ src/init_api.py
@@ -5,5 +5,6 @@ my_app = sly.AppService()
 
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
+PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
 
 api: sly.Api = my_app.public_api
