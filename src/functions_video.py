7b7330f6e7cb8106a0f3dad98593930e10e5932e
Daniil Yarockii <33942379+Lascarfo@users.noreply.github.com>
2021-06-30 12:11:23 +0300

    Update functions_video.py
    Former-commit-id: e0c264f6f98063ef073cb6fae38a3ee80160e745
    Former-commit-id: 5cfe29967c496452f14d8e27cc1fb99590209ff1
    Former-commit-id: 072c7fad9563309a12ef0e5445af57332bc39634
    Former-commit-id: 2d4e87fe38422e0dbf497e6909da9cd8166d6717

--- src/functions_video.py
+++ src/functions_video.py
@@ -153,7 +153,7 @@ def write_frames_to_file(video_name, fps, frames, video_shape, sly_progress=None
     :param video_shape: размер видеокадра
     :return: None
     """
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
     video = cv2.VideoWriter(video_name, fourcc, fps, video_shape)
 
 
