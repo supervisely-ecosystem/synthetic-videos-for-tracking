a4a12cb28c00400489a5b51da960441e2b4a83ce
Daniil Yarockii <yaroc.d@gmail.com>
2021-07-07 12:09:22 +0300

    changing base aug

--- src/augs/base_lite.py
+++ src/augs/base_lite.py
@@ -1,7 +1,7 @@
 import imgaug.augmenters as iaa
 
 seq = iaa.Sequential([
-	iaa.geometric.ScaleX(scale=(0.5, 1.5), order=1, cval=0, mode='constant', fit_output=True),
-	iaa.geometric.ScaleY(scale=(0.5, 1.5), order=1, cval=0, mode='constant', fit_output=True),
+	iaa.geometric.ScaleX(scale=(0.85, 1.15), order=1, cval=0, mode='constant', fit_output=True),
+	iaa.geometric.ScaleY(scale=(0.85, 1.15), order=1, cval=0, mode='constant', fit_output=True),
 	iaa.geometric.Rotate(rotate=(-180, 180), order=1, cval=0, mode='constant', fit_output=True)
 ], random_order=False)
